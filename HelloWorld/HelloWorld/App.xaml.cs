39ff03ffc3e0b650f5f11ff6810c7029986ca99d
George Go <gngo2018@gmail.com>
2020-01-08 08:51:21 -0500

    Added StackPage and GridPage from Udemy Course

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new GreetPage();
+            MainPage = new GridPage();
         }
 
         protected override void OnStart()
