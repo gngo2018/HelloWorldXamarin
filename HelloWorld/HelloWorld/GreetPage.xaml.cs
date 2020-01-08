0f7ac5518fed89a0bd0024306e8ff225b8334aba
George Go <gngo2018@gmail.com>
2020-01-07 22:35:06 -0500

    initial commit

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+
+            // If you want to set styling in code for specific device
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 0, 0);
+                    break;
+            }
+        }
+
+    }
+}
